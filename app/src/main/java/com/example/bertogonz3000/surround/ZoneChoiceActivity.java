3568b0d206a0cddf8ab2fa06f9262110f28952a6
Hannah Park <hannahpark@fb.com>
2018-07-19 13:43:28 -0700

    implement flow between activities through action bar

--- app/src/main/java/com/example/bertogonz3000/surround/ZoneChoiceActivity.java
+++ app/src/main/java/com/example/bertogonz3000/surround/ZoneChoiceActivity.java
@@ -1,8 +1,10 @@
 package com.example.bertogonz3000.surround;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +18,9 @@ public class ZoneChoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zone_choice);
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         //init buttons
         centerButton = findViewById(R.id.centerButton);
         frontLeftButton = findViewById(R.id.frontLeftButton);
@@ -23,6 +28,21 @@ public class ZoneChoiceActivity extends AppCompatActivity {
         backLeftButton = findViewById(R.id.backLeftButton);
         backRightButton = findViewById(R.id.backRightButton);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 
     public void onSelectZone(View view){

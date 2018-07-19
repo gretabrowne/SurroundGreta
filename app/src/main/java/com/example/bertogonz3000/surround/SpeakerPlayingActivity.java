df24131a56d575d60d046d873bf3cac23968e7b7
Greta Browne <gretabrowne@fb.com>
2018-07-19 13:33:34 -0700

    server works

--- app/src/main/java/com/example/bertogonz3000/surround/SpeakerPlayingActivity.java
+++ app/src/main/java/com/example/bertogonz3000/surround/SpeakerPlayingActivity.java
@@ -1,14 +1,22 @@
 package com.example.bertogonz3000.surround;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.parse.ParseLiveQueryClient;
+import com.parse.ParseQuery;
+import com.parse.SubscriptionHandling;
 
 
 public class SpeakerPlayingActivity extends AppCompatActivity {
 
     boolean connected;  //TODO - update this?
+    int songId;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +24,35 @@ public class SpeakerPlayingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_speaker_playing);
         connected = true;
 
+
         //TODO - later make sure the speaker is connected to the master device and server
         if(!connected) {
             Intent intent = new Intent(SpeakerPlayingActivity.this, LostConnectionActivity.class);
             startActivity(intent);
         }
+
+        //        // Make sure the Parse server is setup to configured for live queries
+//        // URL for server is determined by Parse.initialize() call.
+        ParseLiveQueryClient parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient();
+//
+        ParseQuery<Song> query = ParseQuery.getQuery(Song.class);
+        // This query can even be more granular (i.e. only refresh if the entry was added by some other user)
+        // parseQuery.whereNotEqualTo(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+//
+//        // Connect to Parse server]
+        SubscriptionHandling<Song> subscriptionHandling = parseLiveQueryClient.subscribe(query);
+//
+        subscriptionHandling.handleEvent(SubscriptionHandling.Event.CREATE, new SubscriptionHandling.HandleEventCallback<Song>() {
+            @Override
+            public void onEvent(ParseQuery<Song> query, Song object) {
+                // when a new song is "created"
+                Log.d("SpeakerPlayingActivity", "onEvent");
+                songId = object.getFileId();
+                mp = MediaPlayer.create(SpeakerPlayingActivity.this, songId);
+                mp.start();
+            }
+        });
+
     }
 
     //TODO - later make sure the speaker is connected to the master device and server

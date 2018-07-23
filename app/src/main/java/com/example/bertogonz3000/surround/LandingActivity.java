a7a529bac101e68a927bb45579fe743f34e5e0ad
Hannah Park <hannahpark@fb.com>
2018-07-23 12:03:00 -0700

    new landing direction

--- app/src/main/java/com/example/bertogonz3000/surround/LandingActivity.java
+++ app/src/main/java/com/example/bertogonz3000/surround/LandingActivity.java
@@ -1,15 +1,10 @@
 package com.example.bertogonz3000.surround;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.parse.ParseLiveQueryClient;
-import com.parse.ParseQuery;
-import com.parse.SubscriptionHandling;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -31,7 +26,7 @@ public class LandingActivity extends AppCompatActivity {
     //Become a speaker
     public void selectSpeaker(View view){
         //New intent between this and Zone Choice
-        Intent i = new Intent(this, ZoneChoiceActivity.class);
+        Intent i = new Intent(this, ZoneChoiceUpdateActivity.class);
         startActivity(i);
     }
 

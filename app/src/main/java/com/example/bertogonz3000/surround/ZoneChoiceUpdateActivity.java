114306f0a46663dbb7012c7fdd98c017c93e05b8
Hannah Park <hannahpark@fb.com>
2018-07-23 11:56:27 -0700

    continuing from the zone selection

--- app/src/main/java/com/example/bertogonz3000/surround/ZoneChoiceUpdateActivity.java
+++ app/src/main/java/com/example/bertogonz3000/surround/ZoneChoiceUpdateActivity.java
@@ -1,7 +1,9 @@
 package com.example.bertogonz3000.surround;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import it.beppi.knoblibrary.Knob;
@@ -13,6 +15,9 @@ public class ZoneChoiceUpdateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zone_choice_update);
         final Knob knob4 = (Knob) findViewById(R.id.knob4);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         final TextView textView4 = (TextView) findViewById(R.id.textView4);
@@ -28,5 +33,26 @@ public class ZoneChoiceUpdateActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void setLocation() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+//            case R.id.next: {
+//                Intent intent = new Intent(ZoneChoiceUpdateActivity.this, SpeakerPlayingActivity.class);
+//                startActivity(intent);
+//                return true;
+//            }
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }

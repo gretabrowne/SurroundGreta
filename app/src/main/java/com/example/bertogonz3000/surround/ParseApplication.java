22684df40565bf8fd4e8d122bf12472c58a10414
Hannah Park <hannahpark@fb.com>
2018-07-24 10:24:13 -0700

    server

--- app/src/main/java/com/example/bertogonz3000/surround/ParseApplication.java
+++ app/src/main/java/com/example/bertogonz3000/surround/ParseApplication.java
@@ -34,7 +34,7 @@ public class ParseApplication extends Application{
         .applicationId("bghsurround")
         .clientKey(null)
         .clientBuilder(builder)
-        .server("http://172.21.69.54:1337/parse").build());
+        .server("http://172.21.70.174:1337/parse").build());
 
     }
 }

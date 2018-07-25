0ac6843462a1b973ba0377386ed1cf6a1f30bce3
Hannah Park <hannahpark@fb.com>
2018-07-25 16:22:08 -0700

    clock

--- app/src/main/java/com/example/bertogonz3000/surround/SpeakerPlayingActivity.java
+++ app/src/main/java/com/example/bertogonz3000/surround/SpeakerPlayingActivity.java
@@ -146,7 +146,6 @@ public class SpeakerPlayingActivity extends AppCompatActivity {
 
                 } else {
                     changeTime(object.getTime());
-
                 }
 
             }
